266d8ff8e143e7698fcb59b1503b30c9b089070b
Artem Balamutyuk <artyom.balamutyuk1@gmail.com>
2023-09-24 16:35:18 +0300

    feat: Dockerize auth manager app

--- db/init.sql
+++ db/init.sql
@@ -1,1 +1,2 @@
-CREATE DATABASE IF NOT EXISTS dog_walking_service;
+SELECT 'CREATE DATABASE dog_walking_service' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'dog_walking_service')\gexec
+SELECT 'CREATE DATABASE dog_walking_auth_service' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'dog_walking_auth_service')\gexec
